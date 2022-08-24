38bc1a02309d2c2139f89fcdba43b72f65f80222
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-08-24 23:50:54 +0900

    [ADD] TableView 구성 완료(#3)

---
+++ Quiet/Quiet/Screens/Search/Cell/RecentKeywordTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  RecentKeywordTableViewCell.swift
+//  Quiet
+//
+//  Created by SHIN YOON AH on 2022/08/24.
+//
+
+import UIKit
+
+final class RecentKeywordTableViewCell: UITableViewCell {
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
