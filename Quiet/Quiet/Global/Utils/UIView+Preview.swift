8d2ca5b2dedacbfd1177dadb1013d44a411250d4
Minkyeong-Ko <artngmin01@gmail.com>
2022-08-25 01:09:03 +0900

    [ADD] Charts 라이브러리 추가 후 line chart, bar chart 테스트 (#4)

---
+++ Quiet/Quiet/Global/Utils/UIView+Preview.swift
@@ -0,0 +1,28 @@
+//
+//  UIView+Preview.swift
+//  Quiet
+//
+//  Created by Minkyeong Ko on 2022/08/25.
+//
+
+import SwiftUI
+
+extension UIView {
+    public struct UIViewPreview<View: UIView>: UIViewRepresentable {
+        public let view: View
+        public init(_ builder: @autoclosure () -> View) {
+            view = builder()
+        }
+            
+        // MARK: - UIViewRepresentable
+        public func makeUIView(context: Context) -> UIView {
+            return view
+        }
+        public func updateUIView(_ view: UIView, context: Context) {
+        }
+    }
+
+    func toPreview() -> some View {
+        UIViewPreview(self)
+    }
+}
---
+++ Quiet/Quiet/Screens/Detail/NoiseBarChartViewController.swift
@@ -0,0 +1,46 @@
+//
+//  NoiseBarChartViewController.swift
+//  Quiet
+//
+//  Created by Minkyeong Ko on 2022/08/25.
+//
+
+import UIKit
+
+import Charts
+
+class NoiseBarChartViewController: UIViewController {
+    var barChartView: NoiseBarChartView!
+    
+    let months = ["주중", "주말"]
+    let dbValues: [Double] = [68, 54]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupBarChartView()
+    }
+    
+    private func setupBarChartView() {
+        self.barChartView = NoiseBarChartView()
+        self.barChartView.translatesAutoresizingMaskIntoConstraints = false
+        self.barChartView.setupBarChart(dataPoints: months, values: dbValues)
+        self.view.addSubview(self.barChartView)
+        self.barChartView.constraint(
+            top: self.view.topAnchor,
+            leading: self.view.leadingAnchor,
+            bottom: self.view.centerYAnchor,
+            trailing: self.view.trailingAnchor
+        )
+    }
+}
+
+#if DEBUG
+import SwiftUI
+struct NoiseBarChartViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        NoiseBarChartViewController().toPreview().previewInterfaceOrientation(.portrait)
+    }
+}
+#endif
+
