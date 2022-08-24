7f812dab68434f64043a5d0273f80dcc2c2006ea
Minkyeong-Ko <artngmin01@gmail.com>
2022-08-25 01:09:03 +0900

    [ADD] Charts 라이브러리 추가 후 line chart, bar chart 테스트 (#4)

---
+++ Quiet/Quiet/Global/Utils/UIViewController+Preview.swift
@@ -0,0 +1,32 @@
+//
+//  UIViewController+Preview.swift
+//  Quiet
+//
+//  Created by Minkyeong Ko on 2022/08/25.
+//
+
+import UIKit
+import SwiftUI
+
+extension UIViewController {
+    public struct UIViewControllerPreview<ViewController: UIViewController>: UIViewControllerRepresentable {
+        public let viewController: ViewController
+
+        public init(_ builder: @autoclosure () -> ViewController) {
+            viewController = builder()
+        }
+
+        // MARK: - UIViewControllerRepresentable
+        public func makeUIViewController(context: Context) -> ViewController {
+            viewController
+        }
+
+        public func updateUIViewController(_ uiViewController: ViewController, context: UIViewControllerRepresentableContext<UIViewControllerPreview<ViewController>>) {
+
+        }
+    }
+
+    func toPreview() -> some View {
+        UIViewControllerPreview(self)
+    }
+}
