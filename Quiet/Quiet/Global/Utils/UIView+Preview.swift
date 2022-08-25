ee2108fba423888f2fb84c87e163dfa3d00e015e
Minkyeong-Ko <artngmin01@gmail.com>
2022-08-25 10:34:35 +0900

    [FIX] 접근 제한자 수정 #7

--- Quiet/Quiet/Global/Utils/UIView+Preview.swift
+++ Quiet/Quiet/Global/Utils/UIView+Preview.swift
@@ -8,17 +8,17 @@
 import SwiftUI
 
 extension UIView {
-    internal struct UIViewPreview<View: UIView>: UIViewRepresentable {
-        public let view: View
-        public init(_ builder: @autoclosure () -> View) {
+    struct UIViewPreview<View: UIView>: UIViewRepresentable {
+        let view: View
+        init(_ builder: @autoclosure () -> View) {
             view = builder()
         }
             
         // MARK: - UIViewRepresentable
-        public func makeUIView(context: Context) -> UIView {
+        func makeUIView(context: Context) -> UIView {
             return view
         }
-        public func updateUIView(_ view: UIView, context: Context) {
+        func updateUIView(_ view: UIView, context: Context) {
         }
     }
 
