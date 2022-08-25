d2047beffa7f07bff13ae9ea9fb0d570fbaa6c3c
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-08-26 00:00:42 +0900

    [ADD] TableView 적용(#20)

--- Quiet/Quiet/Screens/Search/SheetContainerViewController.swift
+++ Quiet/Quiet/Screens/Search/SheetContainerViewController.swift
@@ -8,8 +8,26 @@
 import UIKit
 
 final class SheetContainerViewController: BaseViewController {
+    
+    // MARK: - Properties
+    
+    private lazy var detailTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.dataSource = self
+        tableView.register(cell: SearchTableViewCell.self)
+        return tableView
+    }()
 
     // MARK: - Func
+    
+    override func setupLayout() {
+        view.addSubview(detailTableView)
+        detailTableView.constraint(top: view.safeAreaLayoutGuide.topAnchor,
+                                   leading: view.leadingAnchor,
+                                   bottom: view.bottomAnchor,
+                                   trailing: view.trailingAnchor,
+                                   padding: UIEdgeInsets(top: 30, left: 0, bottom: 0, right: 0))
+    }
     
     override func configureUI() {
         super.configureUI()
@@ -20,5 +38,18 @@ final class SheetContainerViewController: BaseViewController {
         view.layer.shadowOffset = .init(width: 0, height: -2)
         view.layer.shadowRadius = 20
         view.layer.shadowOpacity = 0.5
+    }
+}
+
+// MARK: - UITableViewDataSource
+extension SheetContainerViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: SearchTableViewCell = tableView.dequeueReusableCell(withType: SearchTableViewCell.self, for: indexPath)
+        cell.setKeyword(to: "더미")
+        return cell
     }
 }
