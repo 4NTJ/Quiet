8731a745dee4e4e0871f90cbcc87bdd6576b15ae
Miller <eotjd8721@naver.com>
2022-08-25 11:18:51 +0900

    [ADD] mapView 및 location using auth 추가

--- Quiet/Quiet/Screens/Map/MapViewController.swift
+++ Quiet/Quiet/Screens/Map/MapViewController.swift
@@ -6,24 +6,89 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+     // MARK: - Properties
+    
+    
+    private let mapView: MKMapView = {
+        let view = MKMapView()
+        view.mapType = MKMapType.standard
+        view.showsUserLocation = true
+        view.setUserTrackingMode(.follow, animated: true)
+        view.isZoomEnabled = true
+        return view
+    }()
+    
+    private let locationManager: CLLocationManager = {
+           let manager = CLLocationManager()
+           manager.desiredAccuracy = kCLLocationAccuracyBest
+           manager.startUpdatingLocation()
+           return manager
+       }()
+    
+    
+   
+    // MARK: - View Life Cycle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupLayout()
+        getLocationUsagePermission()
+        locationManager.delegate = self
+       
     }
+
+    override func viewWillDisappear(_ animated: Bool) {
+            locationManager.stopUpdatingLocation()
+        }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+    // MARK: - Func
+    
+    
+    func setupLayout() {
+        view.addSubview(mapView)
+        mapView.constraint(to: view)
+        
+        
+      
+           }
+    
+    func getLocationUsagePermission() {
+           locationManager.requestWhenInUseAuthorization()
+       }
+    
+  
 
 }
+
+
+    // MARK: - Extension
+
+
+extension MapViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+               switch status {
+               case .authorizedAlways, .authorizedWhenInUse:
+                   print("GPS 권한 설정됨")
+               case .restricted, .notDetermined:
+                   print("GPS 권한 설정되지 않음")
+                   DispatchQueue.main.async {
+                       self.getLocationUsagePermission()
+                   }
+               case .denied:
+                   print("GPS 권한 요청 거부됨")
+                   DispatchQueue.main.async {
+                       self.getLocationUsagePermission()
+                   }
+               default:
+                   print("GPS: Default")
+               }
+           }
+}
+
+
