1925f8679be423de53c2a26c50aed6e581fee37f
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-08-27 06:06:44 +0900

    [FEAT] 데이터 전송을 위한 준비(#65)

--- Quiet/Quiet/Screens/Detail/DetailViewController.swift
+++ Quiet/Quiet/Screens/Detail/DetailViewController.swift
@@ -187,6 +187,23 @@ class DetailViewController: UIViewController {
     }()
     private let scrollContentView = UIView()
     
+    var navigationTitle: String
+    var noiseLevel: NoiseLevel
+    var deviceModel: String
+    
+    // MARK: - Init
+    
+    init(title: String, noiseLevel: NoiseLevel, deviceModel: String) {
+        self.navigationTitle = title
+        self.noiseLevel = noiseLevel
+        self.deviceModel = deviceModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Life cycle
     
     override func viewDidLoad() {
