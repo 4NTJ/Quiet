cb155f5502bb59429811ba54e2b6f4e9a974d7c9
YoonAh-dev <tlsdbsdk05250@gmail.com>
2022-08-26 14:59:58 +0900

    [ADD] Moya Foundation 파일 생성(#33)

---
+++ Quiet/Quiet/Network/Foundation/GeneralAPI.swift
@@ -0,0 +1,13 @@
+//
+//  GeneralAPI.swift
+//  Quiet
+//
+//  Created by SHIN YOON AH on 2022/08/26.
+//
+
+import Foundation
+
+struct GeneralAPI {
+    static let baseURL = "http://api.seoulhackathon.kr"
+    static let apiKey = "U2VvdWxIYWNrYXRob24yMDIy"
+}
